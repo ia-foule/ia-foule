c85dd6972759cdc334bdf001f7ca42351292f7e7
fabricejourdan <jourdan.fabrice@yahoo.fr>
2022-04-22 15:55:39 +0000

    mean std

--- backend/app/mobilecount.py
+++ backend/app/mobilecount.py
@@ -7,8 +7,11 @@ from PIL import Image
 MODEL_NAME = os.getenv("MODEL_NAME_MOBILECOUNT")
 ort_session = onnxruntime.InferenceSession(str(Path('/models/mobilecount') / MODEL_NAME))
 
-mean = np.array([0.452016860247, 0.447249650955, 0.431981861591])
-std = np.array([0.23242045939, 0.224925786257, 0.221840232611])
+#mean = np.array([0.452016860247, 0.447249650955, 0.431981861591])
+#std = np.array([0.23242045939, 0.224925786257, 0.221840232611])
+mean = np.array([0.48879814, 0.4907805, 0.4841541])
+std = np.array([0.22630496, 0.22669446, 0.22931112])
+
 mean = np.float64(mean.reshape(1, -1))
 stdinv = 1 / np.float64(std.reshape(1, -1))
 
